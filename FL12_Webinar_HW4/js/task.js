ffbf62bddfeca3f63d5fc0f3d7d5d9af2deae89b
Mykhaylo Zelisko <zelisko.mykhaylo@gmail.com>
2020-02-23 18:04:36 +0200

    no message

--- FL12_Webinar_HW4/js/task.js
+++ FL12_Webinar_HW4/js/task.js
@@ -13,19 +13,18 @@ function coppyArray(array) {
 // task 3
 
 function addUniqueId(obj) {
-    let newObj = {}
-    const uniqueId = Symbol('uniqId');
-    newObj[uniqueId] = Math.floor( Math.random()*100 );
-    return Object.assign(newObj, obj);
+    const newObj = { ...obj };
+    newObj.uniqueId = Symbol()
+    return newObj
 }
 
 // task 4
 
 function regroupObject(obj) {
-    const {name, ...rest} = obj;
-    const {id, age, university} = rest.details;
-    return {university: university, user: {age: age, firstName: name, id: id}};
-  }
+    const { name, ...rest } = obj;
+    const { id, age, university } = rest.details;
+    return { university: university, user: { age: age, firstName: name, id: id } };
+}
 
 // task 5
 
@@ -48,30 +47,30 @@ function add(a, b = new Error('Missing property')) {
         throw b
     } else {
         return a + b
-    }    
+    }
 }
 
 // task 8
 
 function getMyReposProm(url) {
     fetch(url)
-      .then(response => response.json())
-      .then(myInfo => {
-        const myRepos = myInfo.map(item => item.name);
-        console.log(myRepos);
-      });
-  }
+        .then(response => response.json())
+        .then(myInfo => {
+            const myRepos = myInfo.map(item => item.name);
+            console.log(myRepos);
+        });
+}
 
 // task 9
 
 async function getMyReposAs(url) {
     try {
-      const request = await fetch(url);
-      const myInfo = await request.json();
-      const myRepos = await myInfo.map(item => item.name)
-      console.log(myRepos);
+        const request = await fetch(url);
+        const myInfo = await request.json();
+        const myRepos = await myInfo.map(item => item.name)
+        console.log(myRepos);
     }
     catch (error) {
-      console.log(`ERROR: ${error.stack}`);
+        console.log(`ERROR: ${error.stack}`);
     }
-  }
+}
