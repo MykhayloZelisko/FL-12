e8e858930f8b066fde197cea801674b8815026f1
Mykhaylo Zelisko <zelisko.mykhaylo@gmail.com>
2020-02-26 20:43:55 +0200

    no message

--- FL12_Webinar_HW4/js/task.js
+++ FL12_Webinar_HW4/js/task.js
@@ -4,19 +4,29 @@ function maxElement(array) {
     return Math.max(...array)
 }
 
+const array = [1, 2, 3, 4, 56, 7, 8, 76, 5, 241, 5, 356, 567, 2];
+console.log( maxElement(array) );
+
 // task 2 
 
-function coppyArray(array) {
+function copyArray(array) {
     return [...array]
 }
+
+const array1 = [1, 2, 3];
+const copiedArray = copyArray(array1);
+console.log(array1, copiedArray);
+console.log(array1 === copiedArray);
 
 // task 3
 
 function addUniqueId(obj) {
     const newObj = { ...obj };
-    newObj.uniqueId = Symbol()
+    newObj.id = Symbol()
     return newObj
 }
+
+console.log( addUniqueId({name: 123}) );
 
 // task 4
 
@@ -26,6 +36,17 @@ function regroupObject(obj) {
     return { university: university, user: { age: age, firstName: name, id: id } };
 }
 
+const oldObj = {
+    name: 'Someone',
+    details: {
+        id: 1,
+        age: 11,
+        university: 'UNI'
+    }
+}
+
+console.log( regroupObject(oldObj) );
+
 // task 5
 
 function findUniqueElements(array) {
@@ -34,11 +55,17 @@ function findUniqueElements(array) {
     return Array.from(unique);
 }
 
+const array2 = [1, 1, 23, 3, 4, 5, 6, 5, 4, 23, 2, 1, 1, 1, 1, 1];
+console.log( findUniqueElements(array2) );
+
 // task 6
 
 function hideNumber(str) {
     return str.slice(-4).padStart(str.length, '*');
 }
+
+const phoneNumber = '0123456789';
+console.log( hideNumber(phoneNumber) );
 
 // task 7
 
@@ -49,6 +76,9 @@ function add(a, b = new Error('Missing property')) {
         return a + b
     }
 }
+
+console.log( add(1, 3) );
+// console.log( add(1) );
 
 // task 8
 
@@ -64,6 +94,10 @@ function getMyReposProm(url) {
         });
 }
 
+const url = 'https://jsonplaceholder.typicode.com/users';
+
+console.log( getMyReposProm(url) );
+
 // task 9
 
 async function getMyReposAs(url) {
@@ -77,3 +111,5 @@ async function getMyReposAs(url) {
         console.log(`${error.name}: ${error.message}`);
     }
 }
+
+console.log( getMyReposAs(url) );
